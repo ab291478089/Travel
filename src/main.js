2212fa3f784f17e79aaa80533c797731491822a8
lixiang <lixiang@email.com>
2019-08-17 09:47:42 +0800

    header更新

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'styles/border.css'
+import 'styles/reset.css'
+import 'styles/iconfont.css'
+import fastClick from 'fastClick'
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <header>
+            <div class="left">
+                <div class="iconfont back-btn">&#xe624;</div>
+            </div>
+            <div class="content">
+                <span class="iconfont">&#xe632;</span>
+                请输入内容
+            </div>
+            <div class="right">
+                <span class="iconfont down-btn">&#xe64a;</span>
+                城市
+            </div>
+        </header>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped> 
+@import '~styles/varibles.styl'
+        header 
+            height: .86rem
+            display: flex
+            background-color: $bgnColor
+            line-height:  .86rem
+            color: #fff
+         .left
+            width:  .64rem
+            float:  left 
+            .back-btn 
+                text-align: crnter
+                font-size: .4rem
+            
+        .content 
+            flex:  1
+            line-height: .64rem
+            color: #ccc
+            paddig-left: .2rem
+            height:  .64rem
+            margin-left: .2rem
+            border-radius:  .25rem
+            background-color:  #fff
+            margin-top : .12rem          
+        .right
+            float:  right 
+            width:  1.24rem
+            text-align: center
+            .down-btn
+                     font-size: .2rem
+</style>

6128b1f7a17ae9f6e8da909f63af6e6b37f9c167
xiao <xiao@123.com>
2019-09-16 11:24:01 +0800

    轮播页整合

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,6 +1,11 @@
 <template>
-<div>
-    <img src="//img1.qunarzz.com/sight/p0/1804/1a/1a48cc20b0d6ed44a3.img.jpg_600x330_3f524da7.jpg" alt="" @click="changeGalary">
+<div class="banner">
+    <img  class="banner-img" src="//img1.qunarzz.com/sight/p0/1804/1a/1a48cc20b0d6ed44a3.img.jpg_600x330_3f524da7.jpg" alt="" @click="changeGalary">
+    <div class="banner-photo">
+          <span class="iconfont banner-icon">&#xe692;</span>
+          <em>27</em>
+    </div>
+    <div class="banner-txt">北京欢乐谷(AAAA景区)</div>
     <Gallary :imgs="imgs" v-show="showGallary" @change="hiddenGallary"></Gallary>
 </div>
 </template>
@@ -28,7 +33,30 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-img {
-    width: 100%
-}
+.banner
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+        position: relative
+        width: 100%
+    .banner-photo
+        position: absolute
+        padding: 0 .4rem
+        height: .4rem
+        line-height: .4rem
+        left: .6rem
+        top: 3.5rem
+        background: rgba(0,0,0,.8)
+        color: #fff
+        border-radius: .2rem
+        background-image: linner-gradient(top, rgba(0,0,0,0), rgba(0,0,0,0.8))
+        .banner-icon
+            font-size: .28rem
+            padding: .1rem .1rem
+    .banner-txt
+        position: absolute
+        left: .6rem
+        top: 4rem
+        font-size: .36rem
+        color: #fff
 </style>
